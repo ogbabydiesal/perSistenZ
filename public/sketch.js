d652857aac7279799a81ed522fdcba8fbd8618bd
tommy martinez <thomasjohnmartinez@gmail.com>
2025-12-12 03:18:27 -0500

    fixed panning and sounds

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,14 @@
 
 
 class xClass {  
-    constructor(x, y, name, pic) {
+    constructor(x, y, name, pic, sound) {
         this.x = x;
         this.y = y;
         this.size = 30;
         this.name = name;
         this.playState = false;
         this.pic = pic;
+        this.sound = sound;
     }
     move(newX, newY) {
         this.x = newX;
@@ -16,7 +17,6 @@ class xClass {
     render(x, y) {
         this.x = x;
         this.y = y;
-        
         image(this.pic, this.x , this.y, 100, 100);
     }
  }
@@ -27,40 +27,36 @@ let isTerminal = false;
 
 let positionsJson = {};
 let sources = [];
-
+let sounds = [];
+let panners = [];
 let images = [];
 
 socket.on('invokePersistence', (data) => {
-    console.log('set default positions');
-    console.log(data);
-
     for (const [source, position] of Object.entries(data)) {
-        console.log(source, position);
         positionsJson[source] = position;
-        console.log(position.x);
-        sourcey = new xClass(position.x, position.y, position.name, images[Object.keys(data).indexOf(source)]);
+        sourcey = new xClass(position.x, position.y, position.name, images[Object.keys(data).indexOf(source)], sounds[Object.keys(data).indexOf(source)]);
         sources.push(sourcey);
     }
 });
 
 // ADDED: Listen for position updates from other clients
 socket.on('relaySoundPosition', (data) => {
-    console.log('received position update:', data);
     positionsJson[data.source] = { x: data.x, y: data.y };
 });
 
 function playSound() {
     playing = true;
-    console.log('thishappend');
-    //mySound.play();
-    //mySound.loop();
+    for (let i = 0; i < sounds.length; i++) {
+        sounds[i].play();
+        sounds[i].loop();
+    }
 }
 
 function preload() {
     for (let i = 0; i < 5; i++) {
         images[i] = loadImage(`assets/image${i}.jpg`);
+        sounds[i] = loadSound(`assets/sound${i}.mp3`);
     }
-    mySound = loadSound('assets/gateOpening.mp3');
 }
 
 
@@ -72,10 +68,15 @@ function setup() {
     imageMode(CENTER);
     textFont('Courier New');
     textSize(12);
-    mySound.disconnect();
-    panner = new p5.Panner3D();
-    panner.set(0, 0, 0);
-    mySound.connect(panner);
+
+    for (let i = 0; i < 5; i++) {
+        sounds[i].disconnect();
+        panner = new p5.Panner3D();
+        panners[i] = panner;
+        panner.set(0, 0, 0);
+        sounds[i].connect(panner);
+    }
+
 }
 
 function draw() {
@@ -97,21 +98,30 @@ function draw() {
         
     }
     //update panner position based on source1
-    if (positionsJson['source1']) {
-        let x = map(positionsJson['source1'].x, 0, width, -1, 1);
-        let y = map(positionsJson['source1'].y, 0, height, 1, -1);
-        panner.set(x, y, 0);
+    // if (positionsJson['source1']) {
+    //     let x = map(positionsJson['source1'].x, 0, width, -1, 1);
+    //     let y = map(positionsJson['source1'].y, 0, height, 1, -1);
+    //     panners[0].set(x, y, 0);
+    // }
+
+    for (let i = 0; i < panners.length; i++) {
+        if (positionsJson[`source${i+1}`]) {
+            let x = map(positionsJson[`source${i+1}`].x, 0, width, -1, 1);
+            let y = map(positionsJson[`source${i+1}`].y, 0, height, 1, -1);
+            console.log(`Setting panner${i} to x:${x} y:${y}`);
+            panners[i].set(x, 0, y, 0.1);
+        }
     }
 
     // Draw X-axis (Red)
-  stroke('red'); // Set line color to red
-  strokeWeight(2); // Make the line thicker (optional)
-  line(0, height / 2, width, height / 2); // From left edge to right edge at mid-height
+    stroke('red'); // Set line color to red
+    strokeWeight(2); // Make the line thicker (optional)
+    line(0, height / 2, width, height / 2); // From left edge to right edge at mid-height
 
-  // Draw Y-axis (Blue)
-  stroke('blue'); // Set line color to blue
-  strokeWeight(2); // Make the line thicker (optional)
-  line(width / 2, 0, width / 2, height); // From top edge to bottom edge at mid-width
+    // Draw Y-axis (Blue)
+    stroke('blue'); // Set line color to blue
+    strokeWeight(2); // Make the line thicker (optional)
+    line(width / 2, 0, width / 2, height); // From top edge to bottom edge at mid-width
 
     //check to see if mouse if pressed and over one of the sources
     if (mouseIsPressed || touches.length > 0) {
@@ -120,7 +130,6 @@ function draw() {
             if (d < 30) {
                 //update position
                 positionsJson[source] = { x: mouseX, y: mouseY };
-                console.log(`moved ${source} to ${mouseX}, ${mouseY}`);
                 //send new position to server
                 setSoundPosition(mouseX, mouseY, source);
             }
